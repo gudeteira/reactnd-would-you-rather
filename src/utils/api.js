a63a8f5db5211f7558f5ddaa4b3c97a6509e8c02
pedro.gude <gudeteira@gmail.com>
2019-03-08 16:16:18 +0100

    feat: Add api functions

---
+++ src/utils/api.js
@@ -0,0 +1,23 @@
+import {_getQuestions, _getUsers, _saveQuestion, _saveQuestionAnswer} from './_DATA';
+
+export function getData() {
+  return Promise.all([_getQuestions(), _getUsers()])
+    .then(([questions, users]) => ({
+      questions,
+      users
+    }));
+}
+
+export function saveQuestion(question) {
+  return _saveQuestion(question);
+}
+
+export function answer(data) {
+  return _saveQuestionAnswer(data);
+}
+
+export function formatDate (timestamp) {
+  const d = new Date(timestamp);
+  const time = d.toLocaleTimeString('en-US');
+  return time.substr(0, 5) + time.slice(-2) + ' | ' + d.toLocaleDateString();
+}
