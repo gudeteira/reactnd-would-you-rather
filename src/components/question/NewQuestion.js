f809d406b12eba74afc58d056fd736f63d6677db
pedro.gude <gudeteira@gmail.com>
2019-03-16 16:17:43 +0100

    refactor: Move NewQuestion component to question folder

--- src/components/question/NewQuestion.js
+++ src/components/question/NewQuestion.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {Redirect} from 'react-router-dom';
-import {handleAddQuestion} from '../actions/questions';
+import {handleAddQuestion} from '../../actions/questions';
 
 class NewQuestion extends Component {
   state = {
